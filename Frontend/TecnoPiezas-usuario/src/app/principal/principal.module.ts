62fa0eed067ef39279ef5b111536dc2b10452072
WolfXthaX <135666526+WolfXthaX@users.noreply.github.com>
2023-10-10 19:55:58 -0300

    cambios hechos antes
    cambios al front pagina principal,y routings

--- Frontend/TecnoPiezas-usuario/src/app/principal/principal.module.ts
+++ Frontend/TecnoPiezas-usuario/src/app/principal/principal.module.ts
@@ -2,11 +2,10 @@ import { IonicModule } from '@ionic/angular';
 import { CUSTOM_ELEMENTS_SCHEMA,NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { principalPage } from './principal.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-
-import { TabsPageRoutingModule } from './tabs-routing.module';
-
-import { TabsPage } from './tabs.page';
+import { principalPageRoutingModule } from './principal-routing.module';
 import { register } from 'swiper/element/bundle';
 // register Swiper custom elements
 register();
@@ -16,10 +15,11 @@ register();
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
-  ],
-  declarations: [TabsPage],
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+    ExploreContainerComponentModule,
+    principalPageRoutingModule,
 
+  ],
+  declarations: [principalPage],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
-export class TabsPageModule {}
+export class principalPageModule {}
