f455810f1b30c49e293f63a5c79c05904070aaf1
Bayrpm <bayron_snbk_7@hotmail.com>
2023-10-21 13:15:43 -0300

    Modelo relacional

--- Backend/estructura.sql
+++ Backend/estructura.sql
@@ -26,7 +26,7 @@ CREATE TABLE `api_categoria` (
   `categoria_id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_categoria` varchar(255) NOT NULL,
   PRIMARY KEY (`categoria_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,8 +35,35 @@ CREATE TABLE `api_categoria` (
 
 LOCK TABLES `api_categoria` WRITE;
 /*!40000 ALTER TABLE `api_categoria` DISABLE KEYS */;
-INSERT INTO `api_categoria` VALUES (1,'Tarjetas De Video'),(2,'Procesadores'),(3,'Placas Madre'),(4,'Ram'),(5,'Discos Duros'),(6,'SSD (Unidades De Estado Sólido)'),(7,'Gabinetes'),(8,'Fuentes De Poder'),(9,'Cooler CPU'),(10,'Ventiladores');
+INSERT INTO `api_categoria` VALUES (11,'tarjeta grafica');
 /*!40000 ALTER TABLE `api_categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `api_locales`
+--
+
+DROP TABLE IF EXISTS `api_locales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `api_locales` (
+  `id_locales` int(10) NOT NULL AUTO_INCREMENT,
+  `direccion` varchar(80) NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `correo` varchar(80) NOT NULL,
+  `telefono` varchar(19) NOT NULL,
+  PRIMARY KEY (`id_locales`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `api_locales`
+--
+
+LOCK TABLES `api_locales` WRITE;
+/*!40000 ALTER TABLE `api_locales` DISABLE KEYS */;
+INSERT INTO `api_locales` VALUES (6,'PUGA','SSSSSSS','SS','+56 9 3454 3553');
+/*!40000 ALTER TABLE `api_locales` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -60,7 +87,7 @@ CREATE TABLE `api_producto` (
   KEY `api_producto_subcategoria_id_id_b34300d8` (`subcategoria_id`),
   CONSTRAINT `api_producto_categoria_id_c2e48405_fk_api_categoria_categoria_id` FOREIGN KEY (`categoria_id`) REFERENCES `api_categoria` (`categoria_id`),
   CONSTRAINT `api_producto_subcategoria_id_b2a8d814_fk_api_subca` FOREIGN KEY (`subcategoria_id`) REFERENCES `api_subcategoria` (`subcategoria_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +96,7 @@ CREATE TABLE `api_producto` (
 
 LOCK TABLES `api_producto` WRITE;
 /*!40000 ALTER TABLE `api_producto` DISABLE KEYS */;
-INSERT INTO `api_producto` VALUES (1,'Nvidia RTX4090',890000,35,'La tarjeta gráfica Nvidia GeForce RTX 4090 es una potente unidad de procesamiento gráfico (GPU) diseñada para ofrecer un rendimiento de vanguardia en aplicaciones de gráficos, juegos y computación de alto rendimiento.','productos/rtx4090.jpg',1,1),(2,'RTX 4060',650000,25,'Potente RTX 4060','productos/rtx4060.jpg',1,1);
+INSERT INTO `api_producto` VALUES (3,'rtx',15799,300,'sklkfhfks;jdfhkjgbs','productos/rtx4090.jpg',11,27),(4,'puga',15799,300,'sklkfhfks;jdfhkjgbs','productos/rtx4060_qOBX9JG.jpg',11,27);
 /*!40000 ALTER TABLE `api_producto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,7 +114,7 @@ CREATE TABLE `api_subcategoria` (
   PRIMARY KEY (`subcategoria_id`),
   KEY `api_subcategoria_categoria_id_b2705323_fk_api_categ` (`categoria_id`),
   CONSTRAINT `api_subcategoria_categoria_id_b2705323_fk_api_categ` FOREIGN KEY (`categoria_id`) REFERENCES `api_categoria` (`categoria_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,7 +123,7 @@ CREATE TABLE `api_subcategoria` (
 
 LOCK TABLES `api_subcategoria` WRITE;
 /*!40000 ALTER TABLE `api_subcategoria` DISABLE KEYS */;
-INSERT INTO `api_subcategoria` VALUES (1,'NVIDIA GeForce',1),(2,'AMD Radeon',1),(3,'Intel',2),(4,'AMD',2),(5,'Intel 1200',3),(6,'Intel 1700',3),(7,'AMD AM4',3),(8,'AMD AM5',3),(9,'DDR4 Desktop',4),(10,'DDR5 Desktop',4),(11,'DDR4 Notebook',4),(12,'DDR5 Notebook',4),(13,'Desktop',5),(14,'Notebook',5),(15,'2,5\" SATA',6),(16,'M.2 SATA',6),(17,'M.2 PCIe NVMe',6),(18,'Mini ITX',7),(19,'Micro ATX',7),(20,'ATX',7),(21,'Certificadas',8),(22,'Modulares',8),(23,'Por aire',9),(24,'Refrigeración Líquida',9),(25,'120 mm.',10),(26,'140 mm.',10);
+INSERT INTO `api_subcategoria` VALUES (27,'nvidia',11);
 /*!40000 ALTER TABLE `api_subcategoria` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -176,7 +203,6 @@ CREATE TABLE `auth_permission` (
 
 LOCK TABLES `auth_permission` WRITE;
 /*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
-INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can view log entry',1,'view_logentry'),(5,'Can add permission',2,'add_permission'),(6,'Can change permission',2,'change_permission'),(7,'Can delete permission',2,'delete_permission'),(8,'Can view permission',2,'view_permission'),(9,'Can add group',3,'add_group'),(10,'Can change group',3,'change_group'),(11,'Can delete group',3,'delete_group'),(12,'Can view group',3,'view_group'),(13,'Can add user',4,'add_user'),(14,'Can change user',4,'change_user'),(15,'Can delete user',4,'delete_user'),(16,'Can view user',4,'view_user'),(17,'Can add content type',5,'add_contenttype'),(18,'Can change content type',5,'change_contenttype'),(19,'Can delete content type',5,'delete_contenttype'),(20,'Can view content type',5,'view_contenttype'),(21,'Can add session',6,'add_session'),(22,'Can change session',6,'change_session'),(23,'Can delete session',6,'delete_session'),(24,'Can view session',6,'view_session'),(25,'Can add producto',7,'add_producto'),(26,'Can change producto',7,'change_producto'),(27,'Can delete producto',7,'delete_producto'),(28,'Can view producto',7,'view_producto'),(29,'Can add subcategoria',8,'add_subcategoria'),(30,'Can change subcategoria',8,'change_subcategoria'),(31,'Can delete subcategoria',8,'delete_subcategoria'),(32,'Can view subcategoria',8,'view_subcategoria'),(33,'Can add categoria',9,'add_categoria'),(34,'Can change categoria',9,'change_categoria'),(35,'Can delete categoria',9,'delete_categoria'),(36,'Can view categoria',9,'view_categoria');
 /*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -201,7 +227,7 @@ CREATE TABLE `auth_user` (
   `date_joined` datetime(6) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -210,7 +236,7 @@ CREATE TABLE `auth_user` (
 
 LOCK TABLES `auth_user` WRITE;
 /*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
-INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$600000$krH0NBb2Af1oQXFYYK1e8y$scJnt6bAxLncinfCeBoYHsGNAUVcwokqFzJb1XFGcYo=','2023-10-02 05:06:05.431359',1,'admin','','','admin@admin.com',1,1,'2023-10-02 03:47:14.132326');
+INSERT INTO `auth_user` VALUES (2,'pbkdf2_sha256$600000$fGUaGw2L411473q9mFldXs$ZBNNTRYXOyswKxQmPX5314wFBwkSQ9vqMkGtywICWJY=','2023-10-16 01:26:37.504548',1,'admin','','','admin@admin.com',1,1,'2023-10-16 01:26:04.405917');
 /*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -291,7 +317,7 @@ CREATE TABLE `django_admin_log` (
   KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`),
   CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
   CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -300,7 +326,7 @@ CREATE TABLE `django_admin_log` (
 
 LOCK TABLES `django_admin_log` WRITE;
 /*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
-INSERT INTO `django_admin_log` VALUES (1,'2023-10-02 05:08:15.161184','1','Nvidia RTX4090',1,'[{\"added\": {}}]',7,1),(2,'2023-10-03 02:48:56.211879','2','RTX 4060',1,'[{\"added\": {}}]',7,1),(3,'2023-10-03 02:52:11.829388','1','Nvidia RTX4090',2,'[{\"changed\": {\"fields\": [\"Imagen\"]}}]',7,1);
+INSERT INTO `django_admin_log` VALUES (4,'2023-10-16 01:34:10.408500','1','fafaf',1,'[{\"added\": {}}]',10,2),(5,'2023-10-16 01:36:18.299050','11','tarjeta grafica',1,'[{\"added\": {}}]',12,2),(6,'2023-10-16 01:36:34.335989','27','nvidia',1,'[{\"added\": {}}]',13,2),(7,'2023-10-16 01:36:39.488145','3','puga',1,'[{\"added\": {}}]',11,2),(8,'2023-10-16 02:21:58.302361','4','puga',1,'[{\"added\": {}}]',11,2),(9,'2023-10-16 04:44:51.772573','3','puga',2,'[{\"changed\": {\"fields\": [\"Imagen\"]}}]',11,2),(10,'2023-10-16 04:44:59.862735','3','rtx',2,'[{\"changed\": {\"fields\": [\"Nombre\"]}}]',11,2);
 /*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -317,7 +343,7 @@ CREATE TABLE `django_content_type` (
   `model` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -326,7 +352,7 @@ CREATE TABLE `django_content_type` (
 
 LOCK TABLES `django_content_type` WRITE;
 /*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
-INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(9,'api','categoria'),(7,'api','producto'),(8,'api','subcategoria'),(3,'auth','group'),(2,'auth','permission'),(4,'auth','user'),(5,'contenttypes','contenttype'),(6,'sessions','session');
+INSERT INTO `django_content_type` VALUES (12,'api','categoria'),(10,'api','locales'),(11,'api','producto'),(13,'api','subcategoria');
 /*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -352,7 +378,6 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2023-10-02 03:33:21.955378'),(2,'auth','0001_initial','2023-10-02 03:33:22.261666'),(3,'admin','0001_initial','2023-10-02 03:33:22.334999'),(4,'admin','0002_logentry_remove_auto_add','2023-10-02 03:33:22.342626'),(5,'admin','0003_logentry_add_action_flag_choices','2023-10-02 03:33:22.348651'),(6,'contenttypes','0002_remove_content_type_name','2023-10-02 03:33:22.394098'),(7,'auth','0002_alter_permission_name_max_length','2023-10-02 03:33:22.430617'),(8,'auth','0003_alter_user_email_max_length','2023-10-02 03:33:22.446696'),(9,'auth','0004_alter_user_username_opts','2023-10-02 03:33:22.454009'),(10,'auth','0005_alter_user_last_login_null','2023-10-02 03:33:22.484782'),(11,'auth','0006_require_contenttypes_0002','2023-10-02 03:33:22.486955'),(12,'auth','0007_alter_validators_add_error_messages','2023-10-02 03:33:22.494390'),(13,'auth','0008_alter_user_username_max_length','2023-10-02 03:33:22.509230'),(14,'auth','0009_alter_user_last_name_max_length','2023-10-02 03:33:22.523894'),(15,'auth','0010_alter_group_name_max_length','2023-10-02 03:33:22.540359'),(16,'auth','0011_update_proxy_permissions','2023-10-02 03:33:22.546164'),(17,'auth','0012_alter_user_first_name_max_length','2023-10-02 03:33:22.562420'),(18,'sessions','0001_initial','2023-10-02 03:33:22.585513'),(19,'api','0001_initial','2023-10-02 04:20:21.576362'),(20,'api','0002_alter_producto_categoria_id_and_more','2023-10-02 04:34:37.402002'),(21,'api','0003_rename_categoria_id_producto_categoria_and_more','2023-10-02 05:35:40.854516');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -378,7 +403,7 @@ CREATE TABLE `django_session` (
 
 LOCK TABLES `django_session` WRITE;
 /*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
-INSERT INTO `django_session` VALUES ('uut1i4vtdr0fgdi9b0fzpakjv5viouvy','.eJxVjEEOwiAQRe_C2pAMhTK4dO8ZyMCMUjWQlHbVeHdt0oVu_3vvbyrSupS4dpnjxOqsQJ1-t0T5KXUH_KB6bzq3usxT0ruiD9r1tbG8Lof7d1Col2_tUQC9CUCGrDeDEwQa0xASWxArIaAXIczgDAdBBsku8GgzI9KN1fsDz8Q4Jg:1qnB89:hRctr7CJbAOctryIfbNbzpw-1hFU8kMQ8oRFnC195sA','2023-10-16 05:06:05.433382');
+INSERT INTO `django_session` VALUES ('kw5tdiomusktl75zsjjj39zkrqa3p5p0','.eJxVjMEOwiAQRP-FsyG7VCh49O43EHYBqRpISnsy_rtt0oNe5jDvzbyFD-tS_NrT7KcoLkKJ029HgZ-p7iA-Qr03ya0u80RyV-RBu7y1mF7Xw_07KKGXbW3U2SjOyWEcXbYIw4CEhjUrGjTqZByGPAIhWMsmg8sAiiBYVLSF-HwBwoU2-g:1qsCNR:CeBDurvkRK-jr-cNo0XZIkg1PZb6ft3cv2izaQ6JIcY','2023-10-30 01:26:37.513896');
 /*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -391,4 +416,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-10 18:59:45
+-- Dump completed on 2023-10-16 19:38:22
