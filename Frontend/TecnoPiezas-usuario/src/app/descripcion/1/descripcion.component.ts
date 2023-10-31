160427da639c1a3711bc586ff33e2fdcf6764e29
cristovgm99 <93166555+cristovgm99@users.noreply.github.com>
2023-10-31 05:11:56 -0300

    Pagina descrpcion de producto (beta)

---
+++ Frontend/TecnoPiezas-usuario/src/app/descripcion/1/descripcion.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductosService } from 'src/app/productos.service';
+import { Producto } from 'src/app/model/ClProducto';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-descripcion',
+  templateUrl: './descripcion.component.html',
+  styleUrls: ['./descripcion.component.scss'],
+})
+export class DescripcionComponent  implements OnInit {
+  precio: number;
+  images: string[] = [
+    'https://media.spdigital.cl/file_upload/Mobile_Hero_2_2bdd4d61.png',
+    'https://media.spdigital.cl/file_upload/Mobile_Hero_3_40708f1a.png',
+    'https://media.spdigital.cl/file_upload/Mobile_Hero_3_40708f1a.png',
+    // Agrega más URL de imágenes aquí
+  ];
+
+  isDescriptionVisible = true;
+  isSpecsVisible = true;
+  isWarrantyVisible = true;
+  isContactVisible = true;
+
+   // Agrega métodos para alternar la visibilidad
+   toggleDescription() {
+    this.isDescriptionVisible = !this.isDescriptionVisible;
+  }
+
+  toggleSpecs() {
+    this.isSpecsVisible = !this.isSpecsVisible;
+  }
+
+  toggleWarranty() {
+    this.isWarrantyVisible = !this.isWarrantyVisible;
+  }
+
+  toggleContact() {
+    this.isContactVisible = !this.isContactVisible;
+  }
+
+  productos: Producto[] = [];
+
+  constructor(private productosService: ProductosService, private router: Router) { 
+    this.precio = 999.99;
+  }
+
+  ngOnInit(): void {
+    this.cargarProductos();
+  }
+
+  cargarProductos() {
+    this.productosService.obtenerTodosLosProductos().subscribe((data) => {
+      this.productos = data || [];
+    });
+  }
+
+  agregarAlCarrito(producto: Producto) {
+    // Asegúrate de que 'producto' sea un objeto válido antes de llamar a esta función
+    this.productosService.agregarAlCarrito(producto);
+  }
+
+}
--- Frontend/TecnoPiezas-usuario/src/app/principal/principal-routing.module.ts
+++ Frontend/TecnoPiezas-usuario/src/app/principal/principal-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalPage } from './principal.page';
+import { DescripcionComponent } from '../descripcion/1/descripcion.component';
+
 
 const routes: Routes = [
   {
     path: '',
     component: PrincipalPage,
-  }
+  },
+  {
+    path: 'descripcion/1', // Nombre de la ruta para la lista de productos
+    component: DescripcionComponent
+  },
+
+
 ];
 
 @NgModule({
