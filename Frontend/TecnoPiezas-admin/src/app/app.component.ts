e5aa23ccb2a4d6a18e10f5231f0fdb7c69865937
Bayrpm <bayron_snbk_7@hotmail.com>
2023-11-20 17:19:54 -0300

    Inicio en app admin
    Hice el inicio con los botones para el admin, falta hacerle routerlink a los botones.

--- Frontend/TecnoPiezas-admin/src/app/app.component.ts
+++ Frontend/TecnoPiezas-admin/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AdminService } from './admin.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -6,11 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    
-    { title: 'Listar', url: '/productos', icon: 'cart' },
-    { title: 'Locales', url: '/locales-home', icon: 'basket' },
-    { title: 'Bodegas', url: '/bodegas-home', icon: 'basket' },
+    { title: 'Inicio', url: '/inicio', icon: 'home' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+
+  usuarioLogeado = false;
+  router: any;
+
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    this.adminService.estaLogeado$.subscribe((logeado) => {
+      this.usuarioLogeado = logeado;
+    });
+  }
+
+  logout() {
+    this.adminService.CerrarSesion().subscribe(
+      () => {
+        
+        this.router.navigate(['/inicio-sesion-privado/']);
+
+        console.log('Logout exitoso');
+      },
+      (error: any) => {
+        // Manejo de errores durante el logout
+        if (error.status === 401) {
+          console.error('Token no válido o expirado');
+          // Realizar alguna acción para notificar al usuario sobre el problema del token (por ejemplo, redirigir a la página de inicio de sesión)
+        } else {
+          console.error('Error en el logout:', error);
+          // Otros errores durante el logout (puedes mostrar un mensaje genérico al usuario)
+        }
+      }
+    );
+  }
 }
